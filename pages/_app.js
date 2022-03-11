4303c568447d46c93dfcdbc88135cf35087ef461
11yrsoper <ahmed.t.elmamlook@gmail.com>
2022-03-11 22:18:50 +0200

    tweaks

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,6 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
-} //ts
+}
 
 export default MyApp;
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,23 +1,21 @@
-import express from "express";
-import apicache from "apicache";
+// import express from "express";
+// import apicache from "apicache";
 import { getCampaignInfo } from "./gofundme.js";
 
 const app = express();
 app.set("json spaces", 3);
 app.set("trust proxy", 1);
-let cache = apicache.middleware;
+// let cache = apicache.middleware;
 
-app.get("/", cache("60 minutes"), async (req, res) => {
+export default async function handler(req, res) {
+	res.setHeader("Cache-Control", "s-maxage=86400");
+
 	if (!!!req.query.url) {
-		res.send("<h1>You forgot the URL dummy</h1>");
+		res.status(418).send("<h1>You forgot the URL dummy</h1>");
 		return;
 	}
 	const { goal, raised, formatted } = await getCampaignInfo(
 		encodeURIComponent(req.query.url.replace('"', ""))
 	);
-	res.status(200).json({ goal, raised, formatted }); //lolss
-});
-
-app.listen(3000, () =>
-	console.log("Example app listening on port http://localhost:3000 !")
-);
+	res.status(200).json({ goal, raised, formatted });
+}
