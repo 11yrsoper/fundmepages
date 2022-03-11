8ff8299d9975cab56d73131a193f927f6cd95029
11yrsoper <ahmed.t.elmamlook@gmail.com>
2022-03-11 22:46:15 +0200

    chrome aws lambda

--- pages/api/getCampaignInfo.js
+++ pages/api/getCampaignInfo.js
@@ -1,10 +1,82 @@
-// import express from "express";
-// import apicache from "apicache";
-import { getCampaignInfo } from "../../external-functions/gofundme.js";
-
-// let cache = apicache.middleware;
+import chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
+// import { getCampaignInfo } from "../../external-functions/gofundme.js";
 
 export default async function handler(req, res) {
+	const getCampaignInfo = (url) => {
+		const browser = await puppeteer.launch({
+			args: process.env.PATH || chromium.args,
+			headless: true,
+			executablePath: await chromium.executablePath,
+		});
+		const page = await browser.newPage();
+		await page.goto(decodeURIComponent(url));
+		const goal = await page.$eval(".text-stat.text-stat-title", (e) =>
+			e.innerText.split(" ")
+		);
+		const raised = await page.$eval(".m-progress-meter-heading", (e) =>
+			e.innerText.split(" ")
+		);
+
+		await browser.close();
+		let symbolToAbbrev = {
+			"€": "EUR",
+			"£": "GBP",
+			"₺": "TRY",
+			$: "USD",
+		};
+
+		if (["USD", "CAD", "AUD", "GBP"].includes(goal[0])) {
+			return {
+				goal: goal[3],
+				raised: raised[0],
+				currency: symbolToAbbrev[goal[3][0]],
+				formatted: {
+					goal: parseFloat(
+						goal[3]
+							.replace("$", "")
+							.replace("€", "")
+							.replace("£", "")
+							.replace(",", "")
+							.replace(",", "")
+					),
+					raised: parseFloat(
+						raised[0]
+							.replace("$", "")
+							.replace("€", "")
+							.replace("£", "")
+							.replace(",", "")
+							.replace(",", "")
+					),
+				},
+			};
+		} else {
+			return {
+				goal: goal[2],
+				raised: raised[0],
+				currency: symbolToAbbrev[goal[2][0]],
+				format: {
+					goal: parseInt(
+						goal[2]
+							.replace("$", "")
+							.replace("€", "")
+							.replace("£", "")
+							.replace(",", "")
+							.replace(",", "")
+					),
+					raised: parseInt(
+						raised[0]
+							.replace("$", "")
+							.replace("€", "")
+							.replace("£", "")
+							.replace(",", "")
+							.replace(",", "")
+					),
+				},
+			};
+		}
+	};
+
 	res.setHeader("Cache-Control", "s-maxage=86400");
 
 	if (!!!req.query.url) {
